f9676a547d4a44c8feb43e1898b085caf1d0869d
darkocejkov <darkocheykov@gmail.com>
2022-08-26 01:26:17 -0400

    readme update and todo

--- darkocejkov/src/Components/FoldPage.js
+++ darkocejkov/src/Components/FoldPage.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 
 export default function FoldPage({content}){
 
+
+    // TODO: LEARN + CREATE some component/interface that allows for animating classes.
+
     return (
         <div className={`transition-all fade-in-top flex justify-center linear-progressive-blur hover:backdrop-blur-lg`}>
             <div className="w-fit h-80 overflow-y-auto p-5">
